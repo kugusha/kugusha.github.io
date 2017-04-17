2e2e0b88065221f76967f7ed59fb1c708fea14ff
kugusha <kugush7@gmail.com>
2017-04-17 17:27:13 +0300

    Some fixes

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 function setBackground () {
+    var hex = $('#hex').val();
+    if (hex.length === 1 && hex !== '#') { 
+        $('#hex').val('#' + hex);
+    } 
 
-  var hex = $('#hex').val();
-  
-  if (hex.length <= 1) {
-      document.body.style.background = "#EDB3B2"; 
-    }
+    if (hex.length <= 1) {
+        document.body.style.background = "#EDB3B2";
+    } 
 
-    if (hex.length >= 1) {
-        if((hex[hex.length - 1].match(/[0-9A-Fa-f]/g)) == null) {
+    for (var i = 1; i < hex.length; ++i) {
+        if((hex[i].match(/[0-9A-Fa-f]/g)) == null) {
             $("p").remove();
             $("<p>Invalid hex code</p>").appendTo(".error_m").fadeOut(3000);
             document.body.style.background = "#F9CDAE";
-            hex = hex.slice(0, hex.length - 1);
-            $('#hex').val(hex);
-        } else {
+            $('#hex').val(hex.slice(0,-1));
+        }  else {
             document.body.style.background = hex;
         }
-    }
-
-    if (hex.length === 1 && hex !== '#') { 
-        hex = $('#hex').val('#' + hex);
     }    
 }
+
+$(window).on('load', function TitleChange () {
+      var message = "Don't forget to check it...";
+      var original;
+
+      $(window).focus(function() {
+          if (original) {
+              document.title = original;
+          }
+
+      }).blur(function() {
+          var title = $('title').text();
+          if (title != message) {
+                original = title;
+          }
+          document.title = message;
+      });
+})
