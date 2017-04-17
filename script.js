2d8f00d4b5138c60d164c1539a9061a39b3d2a97
kugusha <kugush7@gmail.com>
2017-04-17 14:06:11 +0300

    bug fixed

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 function setBackground () {
-	var hex = $('#hex').val();
-  	if (hex.length === 1 && hex !== '#') { 
-    	$('#hex').val('#' + hex);
-  	} 
 
-  	if (hex.length <= 1) {
-  		document.body.style.background = "#EDB3B2";
-  	} 
+  var hex = $('#hex').val();
+  
+  if (hex.length <= 1) {
+      document.body.style.background = "#EDB3B2"; 
+    }
 
-  	for (var i = 1; i < hex.length; ++i) {
-     	if((hex[i].match(/[0-9A-Fa-f]/g)) == null) {
+    if (hex.length >= 1) {
+        if((hex[hex.length - 1].match(/[0-9A-Fa-f]/g)) == null) {
             $("p").remove();
-          	$("<p>Invalid hex code</p>").appendTo(".error_m").fadeOut(3000);
-          	document.body.style.background = "#F9CDAE";
-			$('#hex').val(hex.slice(0,-1));
-        }  else {
-        	document.body.style.background = hex;
+            $("<p>Invalid hex code</p>").appendTo(".error_m").fadeOut(3000);
+            document.body.style.background = "#F9CDAE";
+            hex = hex.slice(0, hex.length - 1);
+            $('#hex').val(hex);
+        } else {
+            document.body.style.background = hex;
         }
-  	}  	 
+    }
+
+    if (hex.length === 1 && hex !== '#') { 
+        hex = $('#hex').val('#' + hex);
+    }    
 }
-
-
